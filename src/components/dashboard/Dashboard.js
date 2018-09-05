56815af3709b9b9fc87c2a1270cea4bc48705d25
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2018-09-05 16:59:12 -0600

    dashboard done save position in progress

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import ReactGridLayout from 'react-grid-layout';
+import RGL, { WidthProvider } from "react-grid-layout";
 import Clock from '../widgets/clock/Clock'
 import Dictionary from '../widgets/dictionary/Dictionary'
 import Note from '../widgets/note/Note'
 import Search from '../widgets/search/Search'
 import Weather from '../widgets/weather/Weather'
 import '../../../node_modules/react-grid-layout/css/styles.css'
+const ReactGridLayout = WidthProvider(RGL);
 
 
-export default class Dashboard extends Component {
-  constructor() {
-    super()
-    this.state = {
 
+  
+  export default class Dashboard extends Component {
+    constructor(props) {
+      super(props)
+      this.state={    
+        width: 10,
+      height: 3,
+      layout: []
+      }
+      this.onLayoutChange=this.onLayoutChange.bind(this);
     }
-  }
-
-
-  componentDidMount() {
+    componentDidMount() {
     axios.get('/api/user-data').then(response => {
       console.log('response', response)
       this.props.setUser(response.data);
@@ -33,20 +37,47 @@ export default class Dashboard extends Component {
   }
 
 
+    onLayoutChange(val,i){
+      console.log(val)
+      this.setState({
+        layout: val
+      })
+    }
+  
+    map(){
+      let arr =[]
+      var height = 8;
+      var width = this.state.width;
+      for(let q =0;q<11;q++){
+        arr.push(
+          <div onChange={e=>console.log('gridItem')} style={{backgroundColor: 'hotpink'}} className="gridItem" key={q.toString()} data-grid={{i: q.toString(),x: 0, y: 0, w: width, h: height,}}>we are clones
+          <button onMouseUp={()=>console.log(this.state.layout[q])}>do the things</button>
+          </div>
+        )
+      }
+      return arr;
+    }
+    render() {
+  
+   
+      return (
+        <div >
+          <header >
 
-  render() {
-    return (
-      <div>
-        Ay bruv, this is the Dashboard.
-        <button onClick={() => this.login()}></button>
-        <ReactGridLayout className="layout" cols={30} rowHeight={20} width={1200} height={300}>
-          <div key="1" data-grid={{ i: '1', x: 3, y: 5, w: 3, h: 4 }}><Clock /></div>
-          <div key="2" data-grid={{ i: '2', x: 3, y: 5, w: 3, h: 4 }}><Dictionary /></div>
-          <div key="3" data-grid={{ i: '3', x: 3, y: 5, w: 3, h: 4 }}><Note /></div>
-          <div key="4" data-grid={{ i: '4', x: 3, y: 5, w: 3, h: 4 }}><Search /></div>
-          <div key="5" data-grid={{ i: '5', x: 3, y: 5, w: 3, h: 4 }}><Weather /></div>
-        </ReactGridLayout>
-      </div>
-    )
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+          <div style={{width: '100%', height: '100vh',  overflow: 'scroll'}}>
+          <ReactGridLayout  className="layout" cols={30} rowHeight={5} width={800} height={300}
+                    layout={this.state.layout}
+                    onLayoutChange={this.onLayoutChange}
+                    shouldComponentUpdate={()=>console.log('your gay at' )}
+          >
+  
+            {this.map()}
+          </ReactGridLayout>
+          </div>
+        </div>
+      );
+    }
   }
-}
